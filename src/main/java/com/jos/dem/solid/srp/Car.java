2166bf79853ea230bbad684ef958e95c9ce7f99c
josdem <joseluis.delacruz@gmail.com>
2018-04-17 16:34:56 -0500

    Starting new workshop project in Java :)

---
+++ src/main/java/com/jos/dem/solid/srp/Car.java
@@ -0,0 +1,5 @@
+package com.jos.dem.solid.srp;
+
+public class Car {
+  
+}
---
+++ src/test/java/com/jos/dem/solid/srp/CarSpec.groovy
@@ -0,0 +1,7 @@
+package com.jos.dem.solid.srp
+
+import spock.lang.Specification
+
+class CarSpec extends Specification {
+
+}
