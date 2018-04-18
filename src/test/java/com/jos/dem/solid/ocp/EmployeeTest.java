13b11d96fb893281b31d089f36ac931fd92ef73c
josdem <joseluis.delacruz@gmail.com>
2018-04-18 15:56:51 -0500

    Fixing test to move to follow open close principle

--- src/test/java/com/jos/dem/solid/ocp/EmployeeTest.java
+++ src/test/java/com/jos/dem/solid/ocp/EmployeeTest.java
@@ -3,28 +3,28 @@ package com.jos.dem.solid.ocp;
 import static org.junit.Assert.assertEquals;
 import java.math.BigDecimal;
 import org.junit.Test;
-import com.jos.dem.solid.ocp.EmployeeType;
 
-public class EmployeeTest {
-	
-	private Employee employee = new Employee();
+public class TestEmployee {
 	
 	@Test
 	public void shouldGetEngineerSalary() {
-		BigDecimal salary = new BigDecimal(100.00);
-		assertEquals(salary, employee.getPaymentAmount(EmployeeType.ENGINEER));
+		BigDecimal salary = new BigDecimal(100);
+		Employee engineer = new Engineer();
+		assertEquals(salary, engineer.getPaymentAmount());
 	}
 	
 	@Test
 	public void shouldGetSalesmanSalary() {
-		BigDecimal salary = new BigDecimal(110.00);
-		assertEquals(salary, employee.getPaymentAmount(EmployeeType.SALESMAN));
+		BigDecimal salary = new BigDecimal(110);
+		Employee salesman = new Salesman();
+		assertEquals(salary, salesman.getPaymentAmount());
 	}
 	
 	@Test
 	public void shouldGetManagerSalary() {
-		BigDecimal salary = new BigDecimal(120.00);
-		assertEquals(salary, employee.getPaymentAmount(EmployeeType.MANAGER));
+		BigDecimal salary = new BigDecimal(120);
+		Employee manager = new Manager();
+		assertEquals(salary, manager.getPaymentAmount());
 	}
 
 }
