abc795b0ffbeba2d2f9bffd60431842f2ab42e14
josdem <joseluis.delacruz@gmail.com>
2018-04-18 12:02:18 -0500

    Getting car functionality simpliest

--- src/main/java/com/jos/dem/solid/srp/Car.java
+++ src/main/java/com/jos/dem/solid/srp/Car.java
@@ -2,18 +2,10 @@ package com.jos.dem.solid.srp;
 
 public class Car {
   private static final int MAX_FUEL = 40;
-  private int fuel;
-
-  public Car(){
-    this.fuel = MAX_FUEL;
-  }
+  private int fuel;  
 
   public void fillUp() {
     this.fuel = MAX_FUEL;
-  }
-
-  public int getFuel() {
-    return fuel;
   }
 
   public boolean isFull(){
@@ -22,10 +14,6 @@ public class Car {
 
   public boolean isEmpty(){
     return fuel == 0;
-  }
-
-  public void accelerate() {
-    fuel--;
   }
 
 }
--- src/test/groovy/com/jos/dem/solid/srp/CarSpec.groovy
+++ src/test/groovy/com/jos/dem/solid/srp/CarSpec.groovy
@@ -4,36 +4,20 @@ import spock.lang.Specification
 
 class CarSpec extends Specification {
 
-  void "should start with full tank"(){
+  void "should start with empty tank"(){
     when:'A car'
       Car car = new Car()
     then:'We expect is full of gas'  
-      car.isFull() == true
-  }
-
-  void "should know when is fuel empty"(){
-    given:'A car'
-      Car car = new Car()
-    when:'We expect is full of gas'  
-      emptyFuel(car)
-    then:'Car is low of gas'  
       car.isEmpty() == true
   }
 
   void "should do a gas fill up"(){
     given:'A car'
       Car car = new Car()
-    when:'We expect is full of gas'  
-      emptyFuel(car)
+    when:'We expect is full of gas'        
       car.fillUp()
     then:'Car is low of gas'  
       car.isFull() == true
   }
-
-  private void emptyFuel(Car car){
-    while(car.getFuel() > 0){
-      car.accelerate()
-    }
-  }
-
+  
 }
