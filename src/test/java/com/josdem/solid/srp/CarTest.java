971aa14a90f2d3b6721d72b4e55993b76d4cb462
josdem <joseluis.delacruz@gmail.com>
2024-08-03 12:47:02 -0400

    Setting up the example

--- src/test/java/com/josdem/solid/srp/CarTest.java
+++ src/test/java/com/josdem/solid/srp/CarTest.java
@@ -7,11 +7,19 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class CarTest {
 
+    private final Car car = new Car();
+
     @Test
     @DisplayName("validating a new car")
-    void shouldValidateCar(){
-        var car = new Car();
+    void shouldValidateCar() {
         assertTrue(car.isEmpty(), "should have an empty tank");
+    }
+
+    @Test
+    @DisplayName("filling fuel talk to full")
+    void shouldFuelToFull() {
+        car.fuelToFull();
+        assertTrue(car.isFull(), "should have a full tank");
     }
 
 }
