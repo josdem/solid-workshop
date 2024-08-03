632b044b79de986fe6743863cf3d8ca250366615
josdem <joseluis.delacruz@gmail.com>
2024-08-03 11:33:05 -0400

    #1 Deleting extra line

--- src/test/java/com/josdem/solid/srp/FuelPumpTest.java
+++ src/test/java/com/josdem/solid/srp/FuelPumpTest.java
@@ -6,7 +6,6 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class FuelPumpTest {
-
     private final FuelPump fuelPump = new FuelPump();
 
     @Test
