44eec63ac4e6d6fe04d3bfa1b056a8a80cb13df7
josdem <joseluis.delacruz@gmail.com>
2024-08-03 15:50:33 -0400

    Refactoring test to better names

--- src/test/java/com/josdem/solid/isp/EmployeeTest.java
+++ src/test/java/com/josdem/solid/isp/EmployeeTest.java
@@ -9,27 +9,26 @@ import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-
 public class EmployeeTest {
 
-  @Test
-  public void shouldGetTotalPartnersAmount(){
-    BigDecimal expectedTotal = new BigDecimal(5400);    
-    List<FullTime> partners = Arrays.asList(new FullTimeImpl(10), new FullTimeImpl(15), new FullTimeImpl(20));
+    @Test
+    public void shouldGetTotalFullTimeAmount() {
+        BigDecimal expectedTotal = new BigDecimal(5400);
+        List<FullTime> ftes = Arrays.asList(new FullTimeImpl(10), new FullTimeImpl(15), new FullTimeImpl(20));
 
-    assertEquals(expectedTotal, partners.stream()
-      .map(it -> it.getBaseAmount().add(it.getProfits()))
-      .reduce(BigDecimal.ZERO, BigDecimal::add));
-  }
+        assertEquals(expectedTotal, ftes.stream()
+                .map(it -> it.getBaseAmount().add(it.getProfits()))
+                .reduce(BigDecimal.ZERO, BigDecimal::add));
+    }
 
-  @Test
-  public void shouldGetTotalContractorAmount(){
-    BigDecimal expectedTotal = new BigDecimal(4050);    
-    List<Contractor> contractors = Arrays.asList(new ContractorImpl(10), new ContractorImpl(15), new ContractorImpl(20));
+    @Test
+    public void shouldGetTotalContractorAmount() {
+        BigDecimal expectedTotal = new BigDecimal(4050);
+        List<Contractor> contractors = Arrays.asList(new ContractorImpl(10), new ContractorImpl(15), new ContractorImpl(20));
 
-    assertEquals(expectedTotal, contractors.stream()
-      .map(it -> it.getBaseAmount().add(it.getBonus()))
-      .reduce(BigDecimal.ZERO, BigDecimal::add));
-  }
+        assertEquals(expectedTotal, contractors.stream()
+                .map(it -> it.getBaseAmount().add(it.getBonus()))
+                .reduce(BigDecimal.ZERO, BigDecimal::add));
+    }
 
 }
